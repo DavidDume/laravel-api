900a9af17cfe7503232a8a5476075dc5de65fb18
David Dume <dumedavid11@gmail.com>
2023-06-06 17:35:50 +0200

    aggiunta validazione

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -7,17 +7,37 @@ use App\Mail\NewContact;
 use App\Models\Lead;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 
 class LeadController extends Controller
 {
     public function store(Request $request)
     {
+
         $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'message' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+
         $lead = new Lead();
         $lead->fill($data);
 
         $lead->save();
 
         Mail::to('info@david.it')->send(new NewContact($lead));
+
+        return response()->json([
+            'success' => true
+        ]);
     }
 }
